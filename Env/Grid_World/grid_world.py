83f8ac983b5f4b859c16d44701c40adf4f220a18
safalakkab <safaalakkab7@gmail.com>
2023-02-12 03:59:48 +0100

    f

--- Env/Grid_World/grid_world.py
+++ Env/Grid_World/grid_world.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class GridWorld:
-    def __init__(self, taille=[6, 6], position_start=[0, 1], good_end_position=[4, 2], bad_end_position=[3, 5]):
+    def __init__(self, taille=[4, 4], position_start=[0, 1], good_end_position=[3, 2], bad_end_position=[0, 3]):
         self.current_state = position_start  # État actuel (ligne, colonne)
         self.states = [[x, y] for x in range(taille[0]) for y in range(taille[1])]
         self.end_good_state = good_end_position  # État final (ligne, colonne)
@@ -14,7 +14,7 @@ class GridWorld:
         self.num_actions = 4  # Nombre total d'actions possibles (haut, bas, gauche, droite)
         self.reward = 0  # Récompense actuelle
         self.done = False  # Indique si la partie est terminée
-        self.generate_grid()
+        #self.generate_grid()
         self.actions = [0, 1, 2, 3]
         self.rewards = [0, 1, 3]
         self.actionSpace = {0: -self.grid_size[0], 1: self.grid_size[0],
@@ -51,13 +51,13 @@ class GridWorld:
                 self.current_state[0] = self.grid_size[0] - 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour traverser le mur
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
             else:
                 self.current_state[0] = self.current_state[0] - 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
 
         elif action == 1:
@@ -65,13 +65,13 @@ class GridWorld:
                 self.current_state[0] = 0
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
             else:
                 self.current_state[0] = self.current_state[0] + 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
 
         elif action == 2:
@@ -79,13 +79,13 @@ class GridWorld:
                 self.current_state[1] = self.grid_size[1] - 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
             else:
                 self.current_state[1] = self.current_state[1] - 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
 
         elif action == 3:
@@ -94,13 +94,13 @@ class GridWorld:
                 self.currentIntState = self.getStateInt(self.current_state)
                 # print(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
             else:
                 self.current_state[1] = self.current_state[1] + 1
                 self.currentIntState = self.getStateInt(self.current_state)
                 self.reward = 0  # Pas de récompense pour avancer
-                self.generate_grid()
+                #self.generate_grid()
                 self.endgame()
                 # Si l'on atteint l'état final, la partie est terminée
         return self.currentIntState, self.reward, self.done
